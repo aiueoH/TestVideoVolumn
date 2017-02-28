c1ebf9810b55ae439cf5b59a22776f8745410c98
aiueoH <aiueo.huang.github@gmail.com>
2017-02-28 16:36:39 +0800

    Play video and alarm(video sound) at same time OK

--- app/src/main/java/com/dowob/testvideovolume/MainActivity.java
+++ app/src/main/java/com/dowob/testvideovolume/MainActivity.java
@@ -1,7 +1,14 @@
 package com.dowob.testvideovolume;
 
+import android.content.Context;
+import android.graphics.Paint;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
 import android.net.Uri;
-import android.provider.MediaStore;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +20,7 @@ import java.io.File;
 public class MainActivity extends AppCompatActivity {
 
     private Button mPlayButton;
+    private Button mPlayAlarmAndVideoButton;
     private VideoView mVideoView;
 
     @Override
@@ -26,17 +34,47 @@ public class MainActivity extends AppCompatActivity {
                 playVideo();
             }
         });
+        mPlayAlarmAndVideoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playVideo();
+                playAlarm();
+            }
+        });
     }
 
     private void findViews() {
         mPlayButton = (Button) findViewById(R.id.button_1);
+        mPlayAlarmAndVideoButton = (Button) findViewById(R.id.button_2);
         mVideoView = (VideoView) findViewById(R.id.videoView_1);
+    }
+
+    private void playAlarm() {
+        String path = "android.resource://" + getPackageName() + File.separator + R.raw.rrr;
+        Uri uri = Uri.parse(path);
+        Context context = getApplicationContext();
+        Ringtone r = RingtoneManager.getRingtone(context, uri);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes aa = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MOVIE)
+                    .build();
+            r.setAudioAttributes(aa);
+        } else {
+            r.setStreamType(AudioManager.STREAM_ALARM);
+        }
+        r.play();
     }
 
     private void playVideo() {
         String path = "android.resource://" + getPackageName() + File.separator + R.raw.rrr;
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setVolume(0f, 0f);
+            }
+        });
         mVideoView.setVideoPath(path);
-        mVideoView.requestFocus();
         mVideoView.start();
     }
 }
